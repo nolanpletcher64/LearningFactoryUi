b409141d1f30de924ce25858740278165d1627b5
liu346 <jake17@vt.edu>
2021-10-19 17:44:22 -0400

    change test

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 #endif
 
     qmlRegisterType<BackEnd>("org.qml_cpp_integration.backend",1,0,"BackEnd");
-
+    //tester
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
