00f140b169e3050c8e9bc55e98eca79ec1e4907a
nolanpletcher64 <nolanp@vt.edu>
2021-11-08 17:18:37 -0500

    Merged with JakeTest branch

--- backend.cpp
+++ backend.cpp
@@ -1,6 +1,35 @@
 #include "backend.h"
+#include <QDebug>
+#include <QList>
+#include <QStringList>
+#include <QDir>
+#include <QDebug>
 
-BackEnd::BackEnd(QObject *parent) : QObject(parent)
+BackEnd::BackEnd(QObject *parent) : QObject(parent){}
+QString BackEnd::newLocation()
 {
+    return m_newLocation;
+}
+void BackEnd::setNewLocation(const QString &newLocation)
+{
+    FILE* fpt;
+    if (newLocation == m_newLocation)
+        return;
+    m_newLocation = newLocation;
+    fpt = fopen("data.csv", "a+");
+    fprintf(fpt,"%s\n",newLocation.toUtf8().constData());
+    fclose(fpt);
+    emit newLocationChanged(m_newLocation);
+}
+void BackEnd::logChange(const QString &newLocation){
+    qDebug()<<"New Location:"<<newLocation;
+}
+
+void BackEnd::CSVstuff(const QString &newLocation)
+{
+    FILE* fpt;
+    fpt = fopen("data.csv", "r");
+    fclose(fpt);
+
 
 }
--- backend.h
+++ backend.h
@@ -1,23 +1,23 @@
 #ifndef BACKEND_H
 #define BACKEND_H
-
 #include <QObject>
 #include <QString>
-#include <qqml.h>
-
 class BackEnd : public QObject
 {
     Q_OBJECT
-    QML_ELEMENT
-
+    Q_PROPERTY(QString newLocation READ newLocation WRITE setNewLocation NOTIFY newLocationChanged)
 public:
     explicit BackEnd(QObject *parent = nullptr);
-
+    QString newLocation();
+    Q_INVOKABLE void setNewLocation(const QString &newLocation);
 signals:
+    void newLocationChanged(const QString &newLocation);
 
+public slots:
+    void logChange(const QString &newLocation);
+    void CSVstuff(const QString &newLocation);
 
 private:
-
+    QString m_newLocation;
 };
-
 #endif // BACKEND_H
