229bafc1ade881482e76a26963a608807c3ac597
azizs1 <azizs@vt.edu>
2021-11-10 10:03:43 -0500

    Simple header file to store global ROS vars

---
+++ ros_vars.h
@@ -0,0 +1,15 @@
+#ifndef ROS_VARS_H
+#define ROS_VARS_H
+
+// Battery variables
+extern double bp;
+extern double b_rem;
+
+// Robot Status
+extern std::string status;
+
+// Robot Position
+extern double posx;
+extern double posy;
+
+#endif // ROS_VARS_H
