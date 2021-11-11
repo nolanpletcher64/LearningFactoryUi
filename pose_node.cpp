a8c887f90a08fb20f19ffe8c7d8f3fceaec87093
azizs1 <azizs@vt.edu>
2021-11-11 11:04:22 -0500

    Changed subscribed topic to robot_pose

--- pose_node.cpp
+++ pose_node.cpp
@@ -1,19 +1,19 @@
 #include "ros/ros.h"
-#include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Pose.h"
 #include "ros_vars.h"
 
 double posx;
 double posy;
 
-void poseCallback(const geometry_msgs::Twist& msg) {
-  posx = msg.linear.x;
-  posy = msg.linear.y;
+void poseCallback(const geometry_msgs::Pose::ConstPtr& msg) {
+  posx = msg->position.x;
+  posy = msg->position.y;
 }
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "ReadPose");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("/cmd_vel", 1000, poseCallback);
+  ros::Subscriber sub = n.subscribe("/robot_pose", 1000, poseCallback);
   ros::spin();
   return 0;
 }
